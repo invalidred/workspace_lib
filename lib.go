fa955f4b3bafde64f82e3d081bf5338f6fc08332
invalidred <5270458+invalidred@users.noreply.github.com>
2024-06-10 20:49:05 -0400

    docs: adding doc strings

--- lib.go
+++ lib.go
@@ -1,5 +1,14 @@
+// lib package used to perform arithmetic opeartions
+//
+// You can AddNums and SubNums
 package workspace_lib
 
+// The function adds numbers
 func AddNums(a, b int) int {
 	return a + b
+}
+
+// This funciton subs numbers
+func SubNums(a, b int) int {
+	return a - b
 }
