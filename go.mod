241bdd49c84b14e1d7604d97ff383774fa0c146f
invalidred <5270458+invalidred@users.noreply.github.com>
2024-06-22 20:41:50 -0400

    feat: uses constraints module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/invalidred/workspace_lib
 
 go 1.21.1
+
+require golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8 // indirect
--- lib.go
+++ lib.go
@@ -3,8 +3,18 @@
 // You can AddNums and SubNums
 package workspace_lib
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // The function adds numbers
 func AddNums(a, b int) int {
+	return a + b
+}
+
+func Add[T Number](a, b T) T {
 	return a + b
 }
 
